777802bc7352946d3f984b2436122e930cd8f3a3
007-rohan <122250587+007-rohan@users.noreply.github.com>
2023-03-27 22:16:43 +0530

    Add files via upload

---
+++ DrinkingWaterReminder.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    
+        notification.notify(
+            title = " Drink Water",
+            message = "Just drink Water Brat!! I won't remind you again and again.", 
+            app_icon = "E:\Working With Python\Glass.ico",
+            timeout = 10
+    )
+    # time.sleep(6)    
